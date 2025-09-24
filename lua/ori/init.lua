8305ce7a4e42f3c9e4be815f131454680452fee9
tar80 <tar80master@gmail.com>
2025-09-24 21:28:27 +0900

    feat(bug): custom_highlights was not working

--- lua/ori/init.lua
+++ lua/ori/init.lua
@@ -51,6 +51,9 @@ function ori.load(name)
   vim.go.termguicolors = true
   vim.g.colors_name = 'ori'
   vim.g.ori_palette = palette
+  if type(opts.custom_highlights) == 'function' then
+    opts.custom_highlights = opts.custom_highlights(opts,colors)
+  end
   local custom_highlights = opts.custom_highlights[vim.go.background] or {}
   local highlights = integrations.get_highlights(opts, colors, custom_highlights)
   if #opts.transparent > 0 then
