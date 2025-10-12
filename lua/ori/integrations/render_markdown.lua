0589175a91de8267df204f664258a7bffb505c73
tar80 <tar80master@gmail.com>
2025-10-12 16:41:18 +0900

    feat: adjusted markup heading hlgroups

--- lua/ori/integrations/render_markdown.lua
+++ lua/ori/integrations/render_markdown.lua
@@ -1,12 +1,12 @@
 return function(opts, colors, p)
   p['RenderMarkdownCode'] = { bg = colors.float }
   -- p['RenderMarkdownCodeInline'] = { link = 'RenderMarkdownCode' }
-  p['RenderMarkdownH1Bg'] = { fg = colors.bg, bg = colors.high_orange, style = 'bold' }
-  p['RenderMarkdownH2Bg'] = { fg = colors.bg, bg = colors.high_purple, style = 'bold' }
-  p['RenderMarkdownH3Bg'] = { fg = colors.bg, bg = colors.high_olive, style = 'bold' }
-  p['RenderMarkdownH4Bg'] = { fg = colors.bg, bg = colors.high_green, style = 'bold' }
-  p['RenderMarkdownH5Bg'] = { fg = colors.bg, bg = colors.high_cyan, style = 'bold' }
-  p['RenderMarkdownH6Bg'] = { fg = colors.bg, bg = colors.high_blue, style = 'bold' }
+  p['RenderMarkdownH1Bg'] = { fg = colors.bg, bg = colors.high_cyan, style = 'bold' }
+  p['RenderMarkdownH2Bg'] = { fg = colors.bg, bg = colors.cyan, style = 'bold' }
+  p['RenderMarkdownH3Bg'] = { fg = colors.fg, bg = colors.low_cyan, style = 'bold' }
+  p['RenderMarkdownH4Bg'] = { fg = colors.fg, bg = colors.shade_cyan, style = 'bold' }
+  p['RenderMarkdownH5Bg'] = { fg = colors.fg, bg = colors.shade_cyan, style = 'bold' }
+  p['RenderMarkdownH6Bg'] = { fg = colors.fg, bg = colors.shade_cyan, style = 'bold' }
   p['RenderMarkdownLink'] = { fg = colors.cyan }
   p['RenderMarkdownTodo'] = { link = 'Todo' }
   p['RenderMarkdownDash'] = { fg = colors.shade_gray }
